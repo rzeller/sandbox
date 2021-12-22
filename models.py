dac65bbe50e6525584ad24149f5a5e98f75e1b63
Robby Zeller <rzeller@Robbys-MacBook-Pro.local>
2021-12-21 16:37:02 -0800

    built and tested combined model

--- models.py
+++ models.py
@@ -1,13 +1,14 @@
 import tensorflow as tf
 from tensorflow.keras.utils import to_categorical
 import numpy as np
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D, AvgPool2D, BatchNormalization, Reshape
+from keras.models import Sequential, Model
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D, BatchNormalization, Reshape, Input, Activation, \
+    MaxPooling2D, concatenate
 from sklearn.preprocessing import MinMaxScaler, LabelBinarizer
 
 
-
-def create_cnn(regress=False, summarize=False):
+def create_cnn_sequential(regress=False, summarize=False):
+    # This is the old way of creating the model, which will not work when we combine with the other branch.
     model = Sequential()
     model.add(Conv2D(filters=32, kernel_size=5, activation='relu', input_shape=(28, 28, 1)))
     model.add(MaxPool2D())
@@ -24,37 +25,29 @@ def create_cnn(regress=False, summarize=False):
 
     return model
 
-def create_cnn_x(width, height, depth, filters=(16, 32, 64), summarize=False, regress=False):
+def create_cnn(input_shape, filters=(16, 32, 64), summarize=False, regress=False):
 
-    inputs = Input(shape=(height, width, depth))
+    inputs = Input(shape=input_shape)
 
     for (i, f) in enumerate(filters):
         if i == 0:
             x = inputs
         # CONV => RELU => BN => POOL
-        x = Conv2D(f, (3, 3), padding="same")(x)
-        x = Activation("relu")(x)
+        x = Conv2D(f, (3, 3), padding='same', activation='relu')(x)
         x = BatchNormalization(axis=-1)(x)
         x = MaxPooling2D(pool_size=(2, 2))(x)
 
     # flatten the volume, then FC => RELU => BN => DROPOUT
     x = Flatten()(x)
-    x = Dense(16)(x)
-    x = Activation("relu")(x)
-    x = BatchNormalization(axis=chanDim)(x)
-    x = Dropout(0.5)(x)
-    # apply another FC layer, this one to match the number of nodes
-    # coming out of the MLP
-    x = Dense(4)(x)
-    x = Activation("relu")(x)
+    x = Dense(128, activation='relu')(x)
 
     if regress:
-        x = Dense(1, activation="linear")(x)
+        x = Dense(10, activation="softmax")(x)
 
     model = Model(inputs, x)
 
     if summarize:
-        print(model.summarize())
+        print(model.summary())
 
     return model
 
@@ -89,8 +82,17 @@ def preprocess_features(df, continuous, categorical):
     y_test = df[test]['shipping_fee'].values
     return x_train, y_train, x_test, y_test
 
+def preprocess(df, continuous, categorical=None):
+    images_train, _, images_test, _ = preprocess_images(df)
+    features_train, y_train, features_test, y_test = preprocess_features(df, continuous=continuous,
+                                                                         categorical=categorical)
+    x_train = [images_train, features_train]
+    x_test = [images_test, features_test]
+    return x_train, y_train, x_test, y_test
 
-def create_mlp(num_features, layer_sizes=[32, 16], regress=False, summarize=False):
+
+def create_mlp_sequential(num_features, layer_sizes=[32, 16], regress=False, summarize=False):
+    # This is the old way of creating the model, which will not work when we combine with the other branch.
     model = Sequential()
     model.add(Dense(layer_sizes[0], input_shape=(num_features,), activation='relu'))
     for layer_size in layer_sizes[1:]:
@@ -104,14 +106,36 @@ def create_mlp(num_features, layer_sizes=[32, 16], regress=False, summarize=Fals
     return model
 
 def create_mlp(num_features, layer_sizes=[32, 16], regress=False, summarize=False):
-    model = Sequential()
-    model.add(Dense(layer_sizes[0], input_shape=(num_features,), activation='relu'))
-    for layer_size in layer_sizes[1:]:
-        model.add(Dense(layer_size, activation='relu'))
+    inputs = Input(num_features,)
+
+    for i, layer_size in enumerate(layer_sizes):
+        if i == 0:
+            x = inputs
+        x = Dense(layer_size, activation='relu')(x)
 
     if regress:
-        model.add(Dense(1, activation='linear'))
+        x = Dense(1, activation='linear')(x)
+
+    model = Model(inputs, x)
 
     if summarize:
         print(model.summary())
+    return model
+
+
+def create_combined_model(image_shape, num_features, mlp_layer_sizes=[32, 32], combined_layer_sizes=[32, 32]):
+    cnn = create_cnn(input_shape=image_shape)
+    mlp = create_mlp(num_features=num_features, layer_sizes=mlp_layer_sizes)
+
+    combined_output = concatenate([cnn.output, mlp.output])
+
+    for i, layer_size in enumerate(combined_layer_sizes):
+        if i == 0:
+            x = combined_output
+        x = Dense(4, activation="relu")(x)
+
+    x = Dense(1, activation="linear")(x)
+
+    model = Model(inputs=[cnn.input, mlp.input], outputs=x)
+
     return model
